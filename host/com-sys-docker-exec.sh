3f083f9fc930e7b0e5154f7d563d930bb4786ae0
Stephan <104312932+twcc-dev01@users.noreply.github.com>
2025-04-26 14:21:57 +0200

    Add regex-based update detection in Docker exec script

--- host/com-sys-docker-exec.sh
+++ host/com-sys-docker-exec.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 COM_SYS_DOCKER_PATH="$1"
 DIR=$(dirname "$COM_SYS_DOCKER_PATH")
+REGEX_UPDATE="^update(?: -v (\d+\.\d+\.\d+))?"
+
+echo $DIR
 while true; do
     COMMAND="$(cat $COM_SYS_DOCKER_PATH)"
     echo "$COMMAND" >> "$DIR"/input.log
+    if [[ "$COMMAND" =~ $REGEX_UPDATE ]]; then
+        echo "Starting Update: " "${BASH_REMATCH[1]}"
+    fi
+
     #eval "$(cat $COM_SYS_DOCKER_PATH)";
 done
