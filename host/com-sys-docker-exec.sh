69048e12d3503349b33f58a615b1ec3d66da36b2
Stephan <104312932+twcc-dev01@users.noreply.github.com>
2025-04-26 14:02:29 +0200

    Log Commands to Input File in Docker Exec Script

--- host/com-sys-docker-exec.sh
+++ host/com-sys-docker-exec.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 COM_SYS_DOCKER_PATH="$1"
 while true; do
-    echo ---"$(cat $COM_SYS_DOCKER_PATH)"---
+    COMMAND="$(cat $COM_SYS_DOCKER_PATH)"
+    echo $COMMAND >> input.log
     #eval "$(cat $COM_SYS_DOCKER_PATH)";
 done
