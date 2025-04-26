a97cc76e77dd1be4eb235475505dfabaf64779c4
Stephan <104312932+twcc-dev01@users.noreply.github.com>
2025-04-26 14:05:47 +0200

    Fix log file path to use directory of input file

--- host/com-sys-docker-exec.sh
+++ host/com-sys-docker-exec.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 COM_SYS_DOCKER_PATH="$1"
+DIR=$(dirname "$COM_SYS_DOCKER_PATH")
 while true; do
     COMMAND="$(cat $COM_SYS_DOCKER_PATH)"
-    echo $COMMAND >> input.log
+    echo "$COMMAND" >> "$DIR"/input.log
     #eval "$(cat $COM_SYS_DOCKER_PATH)";
 done
