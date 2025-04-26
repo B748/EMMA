1e233576ab56df43e138cedf23270c701f1459a9
Stephan <104312932+twcc-dev01@users.noreply.github.com>
2025-04-26 15:25:36 +0200

    Use Sudo for Appending to Log File

--- host/com-sys-docker-exec.sh
+++ host/com-sys-docker-exec.sh
@@ -5,7 +5,7 @@ REGEX_UPDATE_PATTERN="^update[[:space:]]?(v[[:digit:]]+\.[[:digit:]]+\.[[:digit:
 
 while true; do
     COMMAND="$(cat $COM_SYS_DOCKER_PATH)"
-    echo "$COMMAND" >> "$DIR"/input.log
+    echo "$COMMAND" | sudo tee -a "$DIR"/input.log > /dev/null
 
     if [[ "$COMMAND" =~ $REGEX_UPDATE_PATTERN ]]; then
         VERSION_TAG=${BASH_REMATCH[1]}
